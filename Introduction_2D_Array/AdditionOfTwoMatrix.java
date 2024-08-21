28f529f421448360c39dffe12e7ceacb88ea8d89
Krishan Mohan <krishanmohan3501@gmail.com>
2024-08-21 23:29:32 +0530

    DSA Stuff

--- Introduction_2D_Array/AdditionOfTwoMatrix.java
+++ Introduction_2D_Array/AdditionOfTwoMatrix.java
@@ -16,8 +16,8 @@ public class AdditionOfTwoMatrix {
         for(int i = 0 ; i < r1;i++){
             for(int j = 0 ; j < c2; j++){
 
-                for(int k = 0; k < r1;k++){
-                    mul[i][j] = a[r1][k] *b[k][c2];
+                for(int k = 0; k < r2;k++){
+                    mul[i][j] += a[r1][k] *b[k][c2];
                 }
 
             }
